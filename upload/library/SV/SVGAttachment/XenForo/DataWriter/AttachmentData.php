a4e07b7f76458a758b0c8adeb573ffe6cf03dc18
Xon <tazipper1@hotmail.com>
2015-10-29 19:16:49 +0800

    Bugfixes

--- upload/library/SV/SVGAttachment/XenForo/DataWriter/AttachmentData.php
+++ upload/library/SV/SVGAttachment/XenForo/DataWriter/AttachmentData.php
@@ -5,7 +5,7 @@ class SV_SVGAttachment_XenForo_DataWriter_AttachmentData extends XFCP_SV_SVGAtta
     protected function _preSave()
     {
         $dimensions = SV_SVGAttachment_Globals::$forcedDimensions;
-        if ($forcedDimensions)
+        if ($dimensions)
         {
             $this->set('width', $dimensions['width']);
             $this->set('height', $dimensions['height']);
@@ -16,14 +16,7 @@ class SV_SVGAttachment_XenForo_DataWriter_AttachmentData extends XFCP_SV_SVGAtta
                 $this->set('thumbnail_width', $dimensions['thumbnail_width']);
                 $this->set('thumbnail_height', $dimensions['thumbnail_height']);
             }
-            if (!file_exists($tempThumbFile) || !is_readable($tempThumbFile))
-            {
-                $this->set('thumbnail_width', 0);
-                $this->set('thumbnail_height', 0);
-
-                $this->setExtraData(self::DATA_TEMP_THUMB_FILE, '');
-                $this->setExtraData(self::DATA_THUMB_DATA, null);
-            }
         }
+        parent::_preSave();
     }
 }
--- upload/library/SV/SVGAttachment/XenForo/Model/Attachment.php
+++ upload/library/SV/SVGAttachment/XenForo/Model/Attachment.php
@@ -10,36 +10,81 @@ class SV_SVGAttachment_XenForo_Model_Attachment extends XFCP_SV_SVGAttachment_Xe
         {
             if (method_exists('XenForo_Helper_DevelopmentXml', 'scanFile'))
             {
-                $svgfile = XenForo_Helper_DevelopmentXml::scanFile($filename);
+                $svgfile = XenForo_Helper_DevelopmentXml::scanFile($file->getTempFile());
             }
             else
             {
-                $svgfile = new SimpleXMLElement($filename, 0, true);
+                $svgfile = new SimpleXMLElement($file->getTempFile(), 0, true);
             }
             $width = substr((string)$svgfile['width'], 0, -2);
             $height = substr((string)$svgfile['height'], 0, -2);
-    
-			$dimensions = array(
-				'width' => $width,
-				'height' => $height,
-			);
+
+            $dimensions = array(
+                'width' => $width,
+                'height' => $height,
+            );
             $tempThumbFile = tempnam(XenForo_Helper_File::getTempDir(), 'xf');
             if ($tempThumbFile)
             {
                 copy($file->getTempFile(), $tempThumbFile);
                 $attachmentThumbnailDimensions = XenForo_Application::getOptions()->attachmentThumbnailDimensions;
-                $dimensions['thumbnail_width'] = ($attachmentThumbnailDimensions > $width) 
+                $dimensions['thumbnail_width'] = ($attachmentThumbnailDimensions > $width)
                                                  ? $attachmentThumbnailDimensions
                                                  : $width;
-                $dimensions['thumbnail_height'] = ($attachmentThumbnailDimensions > $height) 
+                $dimensions['thumbnail_height'] = ($attachmentThumbnailDimensions > $height)
                                                  ? $attachmentThumbnailDimensions
                                                  : $height;
                 SV_SVGAttachment_Globals::$tempThumbFile = $tempThumbFile;
             }
-            
+
             SV_SVGAttachment_Globals::$forcedDimensions = $dimensions;
         }
 
         return parent::insertUploadedAttachmentData($file, $userId, $extra);
+    }
+
+    public function getAttachmentThumbnailFilePath(array $data, $externalDataPath = null)
+    {
+        $extension_swap = null;
+        if (!empty($data['filename']))
+        {
+            $extension = XenForo_Helper_File::getFileExtension($data['filename']);
+            if ($extension == 'svg')
+            {
+                $extension_swap = 'svg';
+
+            }
+        }
+        $path = parent::getAttachmentThumbnailFilePath($data, $externalDataPath);
+        if ($extension_swap)
+        {
+            return $this->_replaceExtenstion($path, $extension_swap);
+        }
+        return $path;
+    }
+
+    public function getAttachmentThumbnailUrl(array $data)
+    {
+        $extension_swap = null;
+        if (!empty($data['filename']))
+        {
+            $extension = XenForo_Helper_File::getFileExtension($data['filename']);
+            if ($extension == 'svg')
+            {
+                $extension_swap = 'svg';
+            }
+        }
+        $url = parent::getAttachmentThumbnailUrl($data);
+        if ($extension_swap)
+        {
+            return $this->_replaceExtenstion($url, $extension_swap);
+        }
+        return $url;
+    }
+
+    private function _replaceExtenstion($path, $ext)
+    {
+        $pos = strrpos($path , '.');
+        return $pos === false ? $path : substr_replace($path, $ext, $pos +1);
     }
 }
